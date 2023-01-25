1bff63024c5e094d75791e172e74e9827163a0fb
dasarijit <dasarijit@users.noreply.github.com>
2023-01-25 22:46:42 +0530

    Add files via upload

--- predict_streamlit.py
+++ predict_streamlit.py
@@ -9,7 +9,81 @@ import streamlit as st
 from PIL import Image
 import numpy as np
 from io import StringIO
+#Import all important packages & libraries
+import numpy as np
+import pandas as pd
+import sklearn
+import re
+from glob import glob
+import xml.etree.ElementTree as ET
+#import glob
+import os
+import json
+
+import matplotlib.gridspec as gridspec
+from matplotlib import pyplot as plt
+%matplotlib inline
+from mpl_toolkits.axes_grid1 import ImageGrid
+import itertools
+import cv2
+import random
+import datetime
+import shutil
+#import wget
+random.seed(1)
+import PIL
+from PIL import Image, ImageDraw
+from zipfile import ZipFile
+import warnings
+warnings.filterwarnings('ignore')
+import matplotlib as mp
+import seaborn as sns
+
+#Import sklearn libraries
+from sklearn import svm, metrics
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+from sklearn.model_selection import learning_curve, GridSearchCV, RandomizedSearchCV
+from sklearn import preprocessing
+from sklearn.metrics import confusion_matrix,classification_report,accuracy_score,roc_auc_score
+from sklearn.preprocessing import LabelBinarizer
+
+#Import tensorflow libraries
 import tensorflow as tf
+from tensorflow.keras import Model
+from tensorflow.keras import optimizers
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import Sequential, Model, load_model
+from tensorflow.keras.layers import Dense,Dropout,Flatten,Conv2D,Convolution2D,MaxPooling2D,MaxPool2D,Activation,GlobalMaxPool2D,GlobalAveragePooling2D,ZeroPadding2D
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.optimizers import Adam,RMSprop
+from tensorflow.keras.preprocessing import image
+#from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.layers import Concatenate, UpSampling2D, Reshape, Input
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint, CSVLogger
+from tensorflow.keras.metrics import Recall, Precision
+from tensorflow.keras.losses import binary_crossentropy, CategoricalCrossentropy
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.applications.inception_v3 import InceptionV3
+from tensorflow.keras.applications.vgg16 import VGG16
+from tensorflow.keras.utils import plot_model
+
+#Import keras libraries
+from keras.applications import xception
+from keras import utils
+from keras.utils.np_utils import to_categorical  
+from keras.utils import np_utils
+
+from tqdm import tqdm
+
+# Print versions
+print(f'Pandas version: {pd.__version__}')
+print(f'Numpy version: {np.__version__}')
+print(f'Scikit-learn version: {sklearn.__version__}')
+print(f'Tensorflow version: {tf.__version__}')
+print(f'CV version: {cv2.__version__}')
 
 
 def footer_markdown():
